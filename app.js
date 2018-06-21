65d35b84e3d4ee93eadcb5fad85862f5e2f2dcc8
Francisco Cortés Hernández <KamuiCortes@MacBook-Pro-de-Francisco.local>
2018-06-21 00:22:21 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+//
 
 // Initialize variables
 var app = express();
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Import routes
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 
 // conection to db
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =>{
@@ -14,12 +29,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 // Rutas
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    ok: true,
-    mensaje: 'Peticion realizada correctamente'
-  })
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 
 // Listen Port
 app.listen(3000, () => {
